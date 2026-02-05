d29feb0166a555ee49438a5848335b17b02ae2a7
Ismael Yasin <ismaelyasin@Mac.communityfibre.co.uk>
2026-02-05 09:44:06 +0000

    Move Dockerfile to docker folder and update workflows

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    # Update bucket name to match your S3 bucket
     bucket         = "terraform-state-aws-cost-dashboard-ismael-yasin"
     key            = "aws-cost-dashboard/terraform.tfstate"
     region         = "eu-west-2"
